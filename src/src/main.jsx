669b74efa777fd90a1a342786e7317dda5df5ce6
xman888 <87570272+xman888@users.noreply.github.com>
2025-07-26 15:31:11 +0300

    Delete src directory

--- src/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import CosmeticsHome from './CosmeticsHome';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <CosmeticsHome />
-  </React.StrictMode>
-);
